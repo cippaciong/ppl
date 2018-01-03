e62ec86df834e141c9e1b112ab5f046d32103a24
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2018-01-03 17:34:54 +0100

    Implement the checkboard function

--- scheme/01_slideshow.rkt
+++ scheme/01_slideshow.rkt
@@ -4,22 +4,34 @@
 (define (square n)
   (filled-rectangle n n))
 
-; define the checker function
+; define the four function to stack four pictures horizontally and vertically
+(define (four p)
+  (define two-p (hc-append p p))
+  (vc-append two-p two-p))
+
+; define the checker function to stack two figures horizontally and vertically
+; swapping them in the bottom row
 (define (checker p1 p2)
   (let ((p12 (hc-append p1 p2))
-        [p21 (hc-append p2 p1)])
+        (p21 (hc-append p2 p1)))
     (vc-append p12 p21)))
-
-; invoke the checker funcion to creare a 2x2 red and black checker
-;(checker (colorize (square 10) "red")
-;         (colorize (square 10) "black"))
 
 ; create a full fledged check board
 ; let's begin defining a board block
 (define block (checker (colorize (square 10) "WhiteSmoke")
                        (colorize (square 10) "Black")))
 ; now let's put them together appending them horizontally and vertically
-(vc-append (checker (hc-append block block)
-                      (hc-append block block))
-             (checker (hc-append block block)
-                      (hc-append block block)))
+(vc-append (hc-append block block block)
+           (hc-append block block block)
+           (hc-append block block block))
+
+; a better implementation is to create a checkboard function using
+; previous functions (checker) and (four)
+(define (checkboard p)
+  (let* ((whitecell (colorize p "WhiteSmoke"))
+         (blackcell (colorize p "Black"))
+         (block (checker whitecell blackcell))
+         (block4 (four block)))
+    (four block4)))
+(checkboard (square 10))
+(checkboard (filled-ellipse 10 10))
